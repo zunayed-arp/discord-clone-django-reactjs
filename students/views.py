c67cbc01afce16269ffeab1c03725c05f57478c3
zunayed-arp <zunayed.arp@gmail.com>
2022-05-03 14:59:07 +0600

    completed part05

--- students/views.py
+++ students/views.py
@@ -1,6 +1,6 @@
 import re
 from django.shortcuts import render
-from .models import Student
+from .models import Student, Teacher
 from django.db import connection
 from django.db.models import Q
 
@@ -34,9 +34,41 @@ def student_list____(request):
     print(connection.queries)
     return render(request, 'output.html', {'posts': obj})
 
-def student_list(request):
+def student_list03(request):
     obj = Student.objects.filter(Q(surname__startswith='ahmed') & Q(firstname__startswith='shamim'))
     print(obj)
     print(obj.query)
     print(connection.queries)
     return render(request, 'output.html', {'posts': obj})
+
+#part04 UNION Query ORM
+def student_list04(request):
+    obj = Student.objects.all().values_list("firstname").union(Teacher.objects.all().values('surname'))
+    print(obj)
+    print(obj.query)
+    print(connection.queries)
+    return render(request, 'output.html', {'posts': obj})
+
+
+#part04 UNION Query ORM
+#gt
+#gte
+#lt
+#lte
+def student_list051(request):
+    # obj = Student.objects.exclude(age=30) & Student.objects.exclude(firstname__startswith='shamim')
+    obj = Student.objects.exclude(age__gt=30)
+    print(obj.query)
+    print(connection.queries)
+    return render(request, 'output.html', {'posts': obj})
+
+def student_list(request):
+    obj = Student.objects.filter(~Q(age__gt=30) & ~Q(surname__startswith='ahmed'))
+    print(obj.query)
+    print(connection.queries)
+    return render(request, 'output.html', {'posts': obj})
+
+
+
+
+
