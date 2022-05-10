8d3a00dcf5a911a8ace09770d585dfb58e2c8b05
zunayed-arp <zunayed.arp@gmail.com>
2022-05-11 01:08:30 +0600

    abstract,multilevel,proxy models

--- students/models.py
+++ students/models.py
@@ -1,5 +1,8 @@
-from pyexpat import model
+
+from venv import create
+from weakref import proxy
 from django.db import models
+from pytz import timezone
 
 
 class Teacher(models.Model):
@@ -18,3 +21,65 @@ class Student(models.Model):
     
     def __str__(self):
         return self.firstname
+    
+    
+#proxy model 
+
+class BookContent(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    
+class BookOrders(BookContent):
+    # objects = NewManager()
+    class Meta:
+        proxy=True
+        ordering = ['created']
+
+    def created_on(self):
+        return timezone.now()-self.created
+
+
+
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    
+class ISBN(Books):
+    books_ptr = models.OneToOneField(
+        Books,on_delete=models.CASCADE,
+        parent_link=True,
+        primary_key=True,
+    )
+    ISBN = models.TextField()
+
+
+
+
+#abstract Model
+class BaseItem(models.Model):
+    title = models.CharField(max_length=255)
+    created= models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        ordering = ['title']
+        
+class ItemA(BaseItem):
+    content = models.TextField()
+    
+    class Meta(BaseItem.Meta):
+        ordering = ['-created']
+        
+class ItemB(BaseItem):
+    file = models.FileField(upload_to='files')
+
+
+class ItemC(BaseItem):
+    file = models.FileField(upload_to='images')
+    
+
+class ItemD(BaseItem):
+    slug = models.SlugField(max_length=255,unique=True)
