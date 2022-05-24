37ef9265e43c73865c1cba7358e4d1e77cebc398
zunayed-arp <zunayed.arp@gmail.com>
2022-05-25 01:31:45 +0600

    m2m completed

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'm2m'
+    'm2one',
 ]
 
 MIDDLEWARE = [
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- m2one/admin.py
+++ m2one/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Publication,Article
+from .models import Reporter,Article
 
 # Register your models here.
 
 
-admin.site.register(Publication)
+admin.site.register(Reporter)
 admin.site.register(Article)
