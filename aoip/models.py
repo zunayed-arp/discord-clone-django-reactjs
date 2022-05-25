42b65359eb976ee7e2c1933d7fbb66952556b25a
zunayed-arp <zunayed.arp@gmail.com>
2022-05-26 01:35:03 +0600

    many to many fields usess

--- aoip/models.py
+++ aoip/models.py
@@ -2,6 +2,24 @@
 from django.db import models
 
 
+
+class Sauce(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Sandwich(models.Model):
+    name = models.CharField(max_length=100)
+    sauces = models.ManyToManyField(Sauce)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
 class IpList(models.Model):
     ip = models.CharField(max_length=30)
 
